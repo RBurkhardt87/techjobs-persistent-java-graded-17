11987fb89b3b5a1c07f77cb2f874e68fac82c93a
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-11-11 21:28:12 -0600

    Got all the queries added to the correct file. Everything passes.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,40 @@
---Part 1
+--Part 1: List columns and their data types job_seq
 
---Part 2
+-- > id		 : int PK
+-- > employer : VARCHAR(225)
+-- > name	 : VARCHAR(225)
+-- > skills	 : VARCHAR(225)
 
---Part 3
 
---Part 4
+
+--Part 2: write a query to list the names of the employers in St. Louis City.
+          -- Do NOT specify an ordering for the query
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
+
+
+
+--Part 3:  write the SQL statement to remove the job table.
+
+DROP TABLE job;
+
+
+
+--Part 4: write a query to return the names of all skills that are attached to jobs in alphabetical order.
+          -- If a skill does not have a job listed, it should not be included in the results of this query.
+
+-- This is how I personally did it, but test didn't approve
+SELECT skill.name
+FROM job_skills
+INNER JOIN skill ON job_skills.skills_id = skill.id
+ORDER BY name ASC;
+
+-- So this is the query setup to meet test expectations:
+
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
+
